cf77fb4af136fdb3fb271232e9c26f56361e1fef
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-23 11:25:36 +0530

    Connect DB

---
+++ src/index.js
@@ -0,0 +1,72 @@
+// import dotenv from 'dotenv';
+
+// dotenv.config({ path: './.env' });
+// const result = dotenv.config({ path: './.env' });
+
+// if (result.error) {
+//     console.error('Error loading environment variables:', result.error.message);
+// } else {
+//     console.log('Environment variables loaded successfully!');
+// }
+
+
+// const dotenv = require('dotenv');
+
+
+
+// require('dotenv').config({ path: './env' });
+
+// "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+
+
+//     (async () => {
+//         try {
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             app.on("error", (error) => {
+//                 console.error("ERROR: ", err)
+//                 throw error
+//             })
+
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`Server running on port ${process.env.PORT}`)
+//             })
+//         } catch (error) {
+//             console.error("ERROR: ", error)
+//             throw err
+//         }
+//     })()
